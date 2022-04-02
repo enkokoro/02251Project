1e85815f4a4f7a49af08638ac179e1853ba8fa15
Semawit Gebrehiwot <semawit.gebrehiwot@gmail.com>
2022-04-02 18:48:59 -0400

    Completed genetic_algorithm file. Must create test case next

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,49 +1,66 @@
 from random import random
 
 # Roulette wheel based selection
-def selection(population, fitnessScores, size):
+def selection(population, fitnessScores):
     totalFitness = sum(fitnessScores)
     relFitness = [f/totalFitness for f in fitnessScores]
-    prob = [sum(relFitness[:i+1]) for i in range(len(relFitness))] 
-    nextGen = []
-    for i in range(size):
-        cutoff = random
-        for j in range(len(population)):
-            if cutoff <= prob[j]:
-                nextGen.append([population[j]])
-                break
-    return nextGen
+    prob = [sum(relFitness[:i+1]) for i in range(len(relFitness))]
+    randNums = [random() for _ in range(len(population))]
+    selected = []
 
-def geneticAlgorithm (input, crossover, crossoverRate, mutate, mutationRate, fitness, numGenerations):
-    population = [input]
+    for selectionNum in randNums:
+        for i, cutoff in enumerate(prob):
+            if selectionNum <= cutoff:
+                selected.append(population[i])
+                break
+
+    return selected
+
+def recombination(parents, crossover, crossoverRate):
+    nextGen = []
+    lenParents = len(parents)
+    for i in range(0, lenParents, 2):
+        p1 = parents[i]
+
+        if (lenParents <= i + 1): # Odd number of parents
+            nextGen.append(p1)
+        else:
+            p2 = parents[i + 1]
+            if random() <= crossoverRate:
+                nextGen.extend([crossover(p1, p2)])
+            else:
+                nextGen.extend([p1, p2])
+
+def mutation(nextGen, mutate, mutationRate):
+    population = []
+    for p in nextGen:
+        if random() <= mutationRate:
+            population.append(mutate(p))
+        else:
+            population.append(p)
+
+
+
+def geneticAlgorithm(input, crossover, crossoverRate, mutate, mutationRate, fitness, numGenerations):
+    # TODO Assertion about input?
+    assert 0 <= crossoverRate and crossoverRate <= 1.0
+    assert 0 <= crossoverRate and crossoverRate <= 1.0
+    assert 0 < numGenerations
+
+    population = input
     fitnessScores = [fitness(p) for p in population]
     for _ in range(numGenerations):
         # Select parents
-        parents = selection(population, fitnessScores, )
+        parents = selection(population, fitnessScores)
 
         # Recombine
-        nextGen = []
-        lenParents = len(parents)
-        for i in range(0, lenParents, 2):
-            p1 = parents[i]
-            # Handle last step when parents len is odd
-            if (lenParents <= i + 1):
-                p2 = parents[i]
-            else:
-                p2 = parents[i + 1]
-            if random() < crossoverRate:
-                nextGen.append([crossover(p1, p2)])
-            else:
-                nextGen.append([p1, p2])
+        nextGen = recombination(parents, crossover, crossoverRate)
 
         # Mutate
-        for p in nextGen:
-            if random() < mutationRate:
-                mutate(p)
+        population = mutation(nextGen, mutate, mutationRate)
         
-        # Update population and recalculate fitness
-        population = nextGen
-        fitnessScores = [fitness(solution) for solution in population]
+        # Recalculate fitness
+        fitnessScores = [fitness(p) for p in population]
 
     # Return fittest member of the population
     best = max(fitnessScores)
