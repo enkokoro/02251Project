37bd6d481b11c8a1700fdce8fdbdeb27f4cee5ca
Evelyn Kuo <evelynkuoev@gmail.com>
2022-04-03 23:31:48 -0400

    update x^2 example to make use of cts_mutate

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 Team Name: SPACE
 Group Members: Semawit Gebrehiwot, Evelyn Kuo, Marina Sha, Lydia Yang
 
-Algorithms:
+Algorithms (algorithms folder):
 - Coral Reef Optimization (coral_reef_optimization.py)
 - Genetic Algorithm (genetic_algorithm.py)
 - Simulated Annealing (simulated_annealing.py)
 
 Applications:
 - Reproduction Operators for Continuous Functions (continuous_fn.py)
-- Rastrigin Function (rastrigin.py)
+- x^2 Function (x^2.py, x^2_results folder)
+- Rastrigin Function (rastrigin.py, rastrigin_results folder)
 - Power Plant Placement Problem (power_plant.py)
--- x^2.py
+++ x^2.py
@@ -1,3 +1,4 @@
+from continuous_fn import cts_mutate
 from algorithms.simulated_annealing import (simulated_annealing, thermodynamic_init_temp, 
                                  thermodynamic_simulated_annealing,
                                  metropolis_hastings_algorithm_probability, 
@@ -7,26 +8,35 @@ import numpy as np
 def square(x):
     return x**2
 
-def real_neighbor(x):
-    return np.random.normal(x, 5)
+def mutate(x):
+    return cts_mutate(x, sigma=5)
 
 folder = "x^2_results/"
 
-print("Temp(x) = 100*x")
-sols, fits, temps = simulated_annealing(100, lambda x: x*100, real_neighbor, metropolis_hastings_algorithm_probability, square, 100)
-print_simulated_annealing(sols, fits, temps, f"{folder}SA_x^2_temp=100x.png")
+"""
+Simulated Annealing
+"""
 print("="*80)
+print("Simulated Annealing")
+print("Temp(x) = 100*x")
+sols, fits, temps = simulated_annealing(100, lambda x: x*100, mutate, metropolis_hastings_algorithm_probability, square, 100)
+print_simulated_annealing(sols, fits, temps, f"{folder}SA_x^2_temp=100x.png")
+print("-"*80)
 
 print("Temp(x) = x")
-sols, fits, temps = simulated_annealing(100, lambda x: x, real_neighbor, metropolis_hastings_algorithm_probability, square, 100)
+sols, fits, temps = simulated_annealing(100, lambda x: x, mutate, metropolis_hastings_algorithm_probability, square, 100)
 print_simulated_annealing(sols, fits, temps, f"{folder}SA_x^2_temp=x.png")
-print("="*80)
+print("-"*80)
 
 print("Temp(x) = e^x-1")
-sols, fits, temps = simulated_annealing(100, lambda x: np.exp(x)-1, real_neighbor, metropolis_hastings_algorithm_probability, square, 100)
+sols, fits, temps = simulated_annealing(100, lambda x: np.exp(x)-1, mutate, metropolis_hastings_algorithm_probability, square, 100)
 print_simulated_annealing(sols, fits, temps, f"{folder}SA_x^2_temp=e^x-1.png")
-print("="*80)
 
+"""
+Thermodynamic Simulated Annealing
+"""
+print("="*80)
+print("Thermodynamic Simulated Annealing")
 def generate_random_number():
     return np.random.uniform(-100, 100)
 
@@ -34,5 +44,5 @@ high_prob = 0.8
 init_temp = thermodynamic_init_temp(100, high_prob, square, generate_random_number)
 k_A = 0.1
 print(f"Thermodynamic SA init_temp = {init_temp} k_A = {k_A}")
-sols, fits, temps = thermodynamic_simulated_annealing(generate_random_number(), init_temp, k_A, real_neighbor, metropolis_hastings_algorithm_probability, square, 100)
+sols, fits, temps = thermodynamic_simulated_annealing(generate_random_number(), init_temp, k_A, mutate, metropolis_hastings_algorithm_probability, square, 100)
 print_simulated_annealing(sols, fits, temps, f"{folder}TSA_x^2_kA=1.png")
