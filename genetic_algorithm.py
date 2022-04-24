f4d86de11ca451f185edbac92b60be7324016d26
Semawit Gebrehiwot <semawit.gebrehiwot@gmail.com>
2022-04-24 13:02:07 -0400

    Added elite and made some changes throughout

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,10 +1,14 @@
 from random import random
+from copy import copy
 import matplotlib.pyplot as plt
 
 # Roulette wheel based selection
 def selection(population, fitnessScores):
     assert len(fitnessScores) == len(population)
 
+    # maxFitness = max(fitnessScores)
+    fit = fitnessScores
+    fitnessScores = [1/f for f in fitnessScores]  # do this to minimize fitness
     totalFitness = sum(fitnessScores)
     relFitness = [f/totalFitness for f in fitnessScores]
     prob = [sum(relFitness[:i+1]) for i in range(len(relFitness))]
@@ -16,7 +20,9 @@ def selection(population, fitnessScores):
             if selectionNum <= cutoff:
                 selected.append(population[i])
                 break
-
+    # if (len(population) != len(selected)):
+    #     print(population, "\n", fitnessScores, "\n", fit, "\n", selected)
+    #     print(len(population), len(selected))
     assert len(population) == len(selected)
     return selected
 
@@ -32,7 +38,7 @@ def recombination(parents, crossover, crossoverRate):
         else:
             p2 = parents[i + 1]
             if random() <= crossoverRate:
-                nextGen.extend(list(crossover(p1, p2)))
+                nextGen.extend(crossover(p1, p2))
             else:
                 nextGen.extend([p1, p2])
 
@@ -53,23 +59,9 @@ def mutation(nextGen, mutate, mutationRate):
 def findBest(fitnessScores, population):
     assert len(fitnessScores) == len(population)
 
-    best = max(fitnessScores)
+    best = min(fitnessScores)
     bestIndex = fitnessScores.index(best)
     return (population[bestIndex], best)
-
-# def findAve(fitnessScores, population):
-#     # assert len(fitnessScores) == len(population)
-
-#     # sortedfitnessScores = sorted(enumerate(fitnessScores), )
-#     # index, _ = sortedfitnessScores[len(sortedfitnessScores)/2]
-#     return sum(population)/len(population)
-
-# def findWorst(fitnessScores, population):
-#     assert len(fitnessScores) == len(population)
-
-#     worst = min(fitnessScores)
-#     worstIndex = fitnessScores.index(worst)
-#     return population[worstIndex]
 
 
 def geneticAlgorithm(population, crossover, crossoverRate, mutate, mutationRate, fitness, numGenerations):
@@ -82,8 +74,28 @@ def geneticAlgorithm(population, crossover, crossoverRate, mutate, mutationRate,
     historyWorst = []
     historyAve = []
     fitnessScores = [fitness(p) for p in population]
+    eliteCount = max(len(population) // 20, 1)
 
     for i in range(numGenerations):
+        # Save history of solutions at the start of each generation
+        historyBest.append(findBest(fitnessScores, population))
+        historyAve.append(sum(fitnessScores)/len(fitnessScores))
+        historyWorst.append(max(fitnessScores))
+
+        # Save "elites"
+        tmp = sorted(enumerate(fitnessScores), key=lambda x: x[1])[:eliteCount]
+        elites = [population[i] for (i,f) in tmp]
+        lenBefore = len(population)
+        # tmpPop = copy(population)
+        # for e in elites: 
+        #     i = population.index(e)
+        #     population.pop(i)
+        #     fitnessScores.pop(i)
+        # assert len(elites) + len(population) == lenBefore
+        assert len(elites) == eliteCount
+        # assert set(elites + population) == set(tmpPop)
+        # print (elites, population)
+
         # Select parents
         parents = selection(population, fitnessScores)
 
@@ -92,17 +104,27 @@ def geneticAlgorithm(population, crossover, crossoverRate, mutate, mutationRate,
 
         # Mutate
         population = mutation(nextGen, mutate, mutationRate)
+
+        # remove worst and add in elites if elites were removed
+        for e in elites:
+            i = fitnessScores.index(max(fitnessScores))
+            fitnessScores.pop(i)
+            population.pop(i)
+            population.append(e)
+            fitnessScores.append(fitness(e))
+                
         
         # Recalculate fitness
         fitnessScores = [fitness(p) for p in population]
 
-        # Save history of solutions at each generation
-        historyBest.append(findBest(fitnessScores, population))
-        historyAve.append(sum(fitnessScores)/len(fitnessScores))
-        historyWorst.append(min(fitnessScores))
+    # Save history of solutions at the last generation
+    historyBest.append(findBest(fitnessScores, population))
+    historyAve.append(sum(fitnessScores)/len(fitnessScores))
+    historyWorst.append(max(fitnessScores))
 
     # Return fittest member of the population along with additional data
-    return historyBest[-1][0], population, historyBest, historyAve, historyWorst
+    solution = historyBest[-1][0]
+    return solution, historyBest, historyAve, historyWorst
 
 
 def printGA(bestTuples, avg, worst, filename):
@@ -116,6 +138,7 @@ def printGA(bestTuples, avg, worst, filename):
 
     ax1.set_xlabel('Generations')
     ax1.set_ylabel('Fitness')
+    plt.yscale("log")
     ax1.plot(best, color=bestColor, label='Best fitness')
     ax1.plot(avg, color=avgColor, label='Average fitness')
     ax1.plot(worst, color=worstColor, label='Worst fitness')
@@ -127,4 +150,4 @@ def printGA(bestTuples, avg, worst, filename):
 
 
     print("Final Solution: ", solutions[-1], " Fitness: ", best[-1])
-    print("Best Solution: ", solutions[best.index(max(best))], " Fitness: ", max(best))
+    print("Best Solution: ", solutions[best.index(min(best))], " Fitness: ", min(best))
