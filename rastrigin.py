073e0124c6dbbd12a8545dadb5531c9f17b25ac6
Evelyn Kuo <evelynkuoev@gmail.com>
2022-05-05 10:29:20 -0400

    fixed up rastrigin.py to run for 10 times and report statistics of the runs

--- rastrigin.py
+++ rastrigin.py
@@ -37,10 +37,6 @@ class Rastrigin():
         unclipped = cts_crossover(p1, p2, num_children=num_children)
         return [np.clip(unclip, -5.12, 5.12) for unclip in unclipped]
 
-def prints(s):
-    if print_alot:
-        print(s)
-
 Ns = [1, 5, 10]
 for n in Ns:
     print("="*80)
@@ -53,75 +49,89 @@ for n in Ns:
     num_generations = 50
     population_size = N * M
     num_runs = 10   # because algorithms are nondeterministic, we did some runs and took average
-    init = [rastrigin.random_feasible_point() for i in range(population_size)]
 
-    """
-    Coral Reef Optimization
-    """
+    final_results = {'CRO': [], 'GA':[], 'SA': [], 'TSA':[]}
+    for run_idx in range(num_runs):
+        init = [rastrigin.random_feasible_point() for i in range(population_size)]
+
+        """
+        Coral Reef Optimization
+        """
+        print("-"*80)
+        print("CRO")
+        p0 = 0.05# 0.01
+        pk = 0.5
+        k = 10
+        Fa = 0.1
+        Fd = Fa
+        Pd = 0.05
+        solution, reef_evolutions = CRO(init, N, M, lambda x: -rastrigin.fitness(x), rastrigin.crossover, rastrigin.mutate, p0, pk, k, Fa, Fd, Pd, num_generations)
+        print(solution)
+        final_results['CRO'].append((solution[0], -solution[1]))
+        if solution[1] <= min([sol[1] for sol in final_results['CRO']]): # save best run
+            visualize_coral_reef_optimization(reef_evolutions, filename=f"rastrigin_n={n}")
+
+        """
+        Genetic Algorithm
+        """
+        print("-"*80)
+        print("Genetic Algorithm")
+        crossoverRate = 0.7
+        mutationRate = 0.032
+
+        res, best, avg, worst = geneticAlgorithm(init, rastrigin.crossover, crossoverRate, rastrigin.mutate, mutationRate, rastrigin.fitness, num_generations)
+        res = np.array(res)
+        final_results['GA'].append((res, rastrigin.fitness(res)))
+        if rastrigin.fitness(res) <= min([sol[1] for sol in final_results['GA']]):
+            printGA(best, avg, worst, f"{rastrigin_folder}GA_n={n}_Pop={population_size}_Gens={num_generations}_crossRate={crossoverRate}_mutRat={mutationRate}.png", 
+                f"Rastrigin - Genetic Algorithm (n = {n})")
+
+        """
+        Simulated Annealing
+        """
+        print("-"*80)
+        print(f"Simulated Annealing")
+        init = rastrigin.random_feasible_point()
+        sols, fits, temps = simulated_annealing(init, lambda x: np.exp(x)-1, rastrigin.mutate, metropolis_hastings_algorithm_probability, 
+            rastrigin.fitness, max_iterations=1000*n)
+        best = min(sols, key=lambda x: rastrigin.fitness(x))
+        print(best)
+        final_results['SA'].append((best, rastrigin.fitness(best)))
+        if rastrigin.fitness(best) <= min([sol[1] for sol in final_results['SA']]):
+            print_simulated_annealing(sols, fits, temps, f"{rastrigin_folder}SA_n={n}_temp=e^x-1.png")
+
+        """
+        Thermodynamic Simulated Annealing
+        """
+        print("-"*80)
+        print("Thermodynamic Simulated Annealing")
+        high_prob = 0.8 #[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        k_A = 0.1 #[0.01, 0.05, 0.1, 0.2, 0.5, 0.75, 0.9]
+
+        init_temp = thermodynamic_init_temp(100, high_prob, rastrigin.fitness, rastrigin.random_feasible_point)
+        sols, fits, temps = thermodynamic_simulated_annealing(init, init_temp, k_A, rastrigin.mutate, metropolis_hastings_algorithm_probability, 
+            rastrigin.fitness, max_iterations=population_size*num_generations*n)
+
+        best = min(sols, key=lambda x: rastrigin.fitness(x))
+        print(best)
+        final_results['TSA'].append((best, rastrigin.fitness(best)))
+        if rastrigin.fitness(best) <= min([sol[1] for sol in final_results['TSA']]):
+            print_simulated_annealing(sols, fits, temps, f"{rastrigin_folder}TSA_n={n}_highprob={high_prob}_kA={k_A}.png")
+
+print("="*80)
+print("SUMMARY OF RUNS")
+print("="*80)
+for algo in final_results:
+    print("Algorithm: ", algo)
+    best_sol, best_fit = min(final_results[algo], key=lambda x: x[1])
+    print("\tBest Solution: ", best_sol)
+    print("\tBest Objective Value: ", best_fit)
+    fitnesses = [sol[1] for sol in final_results[algo]]
+    print("Statistics for fitness")
+    pd.DataFrame(np.array(fitnesses)).describe()
     print("-"*80)
-    print("CRO")
-    p0 = 0.1# 0.01
-    pk = 0.5
-    k = 10
-    Fa = 0.1
-    Fd = Fa
-    Pd = 0.05
-    solution, reef_evolutions = CRO(init, N, M, lambda x: -rastrigin.fitness(x), rastrigin.crossover, rastrigin.mutate, p0, pk, k, Fa, Fd, Pd, num_generations)
-    print(solution)
-    visualize_coral_reef_optimization(reef_evolutions, filename=f"rastrigin_n={n}")
-
-    """
-    Genetic Algorithm
-    """
-    print("-"*80)
-    print("Genetic Algorithm")
-    crossoverRate = 0.7
-    mutationRate = 0.032
-
-    res, best, avg, worst = geneticAlgorithm(init, rastrigin.crossover, crossoverRate, rastrigin.mutate, mutationRate, rastrigin.fitness, num_generations)
-    printGA(best, avg, worst, f"{rastrigin_folder}GA_n={n}_Pop={population_size}_Gens={num_generations}_crossRate={crossoverRate}_mutRat={mutationRate}.png", 
-        f"Rastrigin - Genetic Algorithm (n = {n})")
-
-    """
-    Simulated Annealing
-    """
-    print("-"*80)
-    print(f"Simulated Annealing")
-    init = rastrigin.random_feasible_point()
-    sols, fits, temps = simulated_annealing(init, lambda x: np.exp(x)-1, rastrigin.mutate, metropolis_hastings_algorithm_probability, 
-        rastrigin.fitness, max_iterations=1000*n)
-    print_simulated_annealing(sols, fits, temps, f"{rastrigin_folder}SA_n={n}_temp=e^x-1.png")
-
-    """
-    Thermodynamic Simulated Annealing
-    """
-    print_alot = True
-    print("-"*80)
-    print("Thermodynamic Simulated Annealing")
-    high_probability_ps = [0.8]#[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    k_A_ps = [0.1]#[0.01, 0.05, 0.1, 0.2, 0.5, 0.75, 0.9]
-    results = {}
-    for high_prob in high_probability_ps:
-        results_prob = {}
-        for k_A in k_A_ps:
-            mfit = 0
-            prints("-"*60)
-            prints(f"high_prob: {high_prob} k_A: {k_A}")
-            for _ in range(num_runs):
-                init_temp = thermodynamic_init_temp(100, high_prob, rastrigin.fitness, rastrigin.random_feasible_point)
-                sols, fits, temps = thermodynamic_simulated_annealing(init, init_temp, k_A, rastrigin.mutate, metropolis_hastings_algorithm_probability, 
-                    rastrigin.fitness, max_iterations=population_size*num_generations*n)
-                mfit += min(fits)
-            if print_alot:
-                print_simulated_annealing(sols, fits, temps, f"{rastrigin_folder}TSA_n={n}_highprob={high_prob}_kA={k_A}.png")
-
-            results_prob[k_A] = mfit/num_runs
-        results[high_prob] = results_prob
     
-    prints("-"*80)
-    print("Parameter Search Results")
-    results = pd.DataFrame(results)
 
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-        print(results)
+    
+        
 
