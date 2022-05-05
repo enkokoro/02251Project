29627e4253a617bb3731370890757b422c7145bd
Semawit Gebrehiwot <semawit.gebrehiwot@gmail.com>
2022-05-04 21:19:52 -0400

    Cleaned up. Pulled everything out into helper functions and removed step.

--- algorithms/coral_reef_optimization_rastrigin.py
+++ algorithms/coral_reef_optimization_rastrigin.py
@@ -6,111 +6,15 @@ import random
 #lots of repeats in results (also extremely optimized) - is this because of asexual reproduction???
 #how to use/interpret results, how to deal with/represent colonies of corals
 
-#new coral attempts to settle into reef
-def settle(coral, reef, N, M):
-    i = random.randint(0, N-1)
-    j = random.randint(0, M-1)
-
-    if (reef[i][j] == None):
-        reef[i][j] = coral
-    else:
-        h0 = coral[1]
-        h1 = reef[i][j][1]
-        if (h0 > h1):
-            reef[i][j] = coral
-
-#pk = fraction of broadcast spawners
-#k = number of times corals attempt to settle before giving up
-#fa = fraction of corals that asexually reproduce
-#pd = probability for depredation
-def step(hf, reef, pk, k, fa, pd, N, M, crossover, mutation):
-    #broadcast spawning
-    bsp = [] #broadcast spawners
-    br = [] #brooders
-    corals = [c for row in reef for c in row if c != None]
-
-    for coral in corals:
-        r = random.random()
-        if (r <= pk):
-            bsp.append(coral)
-        else: br.append(coral)
-
-    newCorals = []
-    while (len(bsp)>1):
-        #select couple
-        couple = random.sample(bsp, 2)
-        c1 = couple[0]
-        c2 = couple[1]
-        larva = crossover(c1[0], c2[0], numChildren=1)
-
-        bsp.remove(c1)
-        bsp.remove(c2)
-
-        #make larvae - does health function of larvae depend on health of parents?
-        #can't add larvae into corals yet - haven't successfully settled into reef
-        #child's health = average of parents
-        newCorals.append((larva, hf(larva)))
-
-    #brooding - same question as before
-    for c in br:
-        larva = mutation(c)
-        newCorals.append((larva, hf(larva)))
-
-    #larvae setting
-    for c in newCorals:
-        for j in range(k):
-            settle(c, reef, N, M)
-
-    # TODO come back!! check paper
-    #asexual reproduction
-    ar = []
-    #make a sorting function??? 
-    for c in corals:
-        r = random.random()
-        if (r <= fa):
-            ar.append(c)
-
-    for c in ar:
-        for i in range(k):
-            settle(c, reef)
-
-    #depredation
-    #find health "cutoff" for corals (used to find corals with worst health)
-    healths = []
-    for i in range(N):
-        for j in range(M):
-            if (not reef[i][j] == None):
-                h = reef[i][j][1]
-                healths.append(h)
-
-    healths.sort()
-    mh = healths[round(len(healths)*fa)]
-
-    for i in range(N):
-        for j in range(M):
-            if (not reef[i][j] == None):
-                r = random.random()
-                if (r <= pd):
-                    c = reef[i][j]
-                    if (c[1] <= mh):
-                        reef[i][j] = None
-                        
-
-def CRO(hf, N, M, p0, numIters, init, crossover, mutation, pk, k, fa, pd):
-    assert 0 < p0 and p0 < 1
-    assert 0 < N and 0 < M
-    assert type(init) == list
-    # Each coral is labeled with an associated health function??
-
-    #initialize N * M reef
+def initialize(reef, init, N, M, p0, hf):
+    # initialize N * M reef
     # p0 is a ratio of populated vs unpopulated colonies, not a probability!
     # Example in paper: N=5, M=6, p0=0.43, size=30, pop=9, unpop=21
     size = N * M
     numCorals = round((size * p0) / (1 + p0))
-    indicies = np.random.randint(low=0, high=size, size=numCorals)
-    reef = [[None for _ in range(M)] for _ in range(N)]
+    indices = np.random.randint(low=0, high=size, size=numCorals)
 
-    for i in indicies:
+    for i in indices:
         row = i // M
         col = i % M
         
@@ -122,13 +26,125 @@ def CRO(hf, N, M, p0, numIters, init, crossover, mutation, pk, k, fa, pd):
         # Each coral is a tuple of solution and fitness
         reef[row][col] = (coral, hf(coral)) # TODO need fitness??
 
+def separate(reef, pk):
+    bsp = [] #broadcast spawners
+    br = [] #brooders
+    corals = [c for row in reef for c in row if c != None]
+
+    for coral in corals:
+        r = random.random()
+        if (r <= pk):
+            bsp.append(coral)
+        else: br.append(coral)
+
+    return bsp, br
+
+def broadcastSpawning(bsp, crossover, newCorals, hf):
+    while (len(bsp) > 1):
+        # select couple
+        couple = random.sample(bsp, 2)
+        c1 = couple[0]
+        c2 = couple[1]
+        larva = crossover(c1[0], c2[0], numChildren=1)
+
+        bsp.remove(c1)
+        bsp.remove(c2)
+
+        newCorals.append((larva, hf(larva)))
+
+def brooding(br, mutate, newCorals, hf):
+    for c in br:
+        larva = mutate(c)
+        newCorals.append((larva, hf(larva)))
+
+def larvaeSetting(newCorals, k, reef, N, M)
+    for c in newCorals:
+        for _ in range(k):
+            settle(c, reef, N, M)
+
+def asexualReproduction(reef, Fa, k, N, M):
+    ar = []
+    # TODO make a sorting function???
+    corals = [c for row in reef for c in row if c != None]
+    
+    for c in corals:
+        r = random.random()
+        if (r <= Fa):
+            ar.append(c)
+
+    for c in ar:
+        for _ in range(k):
+            settle(c, reef, N, M)
+
+#new coral attempts to settle into reef
+def settle(coral, reef, N, M):
+    i = random.randint(0, N - 1)
+    j = random.randint(0, M - 1)
+
+    if (reef[i][j] == None):
+        reef[i][j] = coral
+    else:
+        h0 = coral[1]
+        h1 = reef[i][j][1]
+        if (h0 > h1):
+            reef[i][j] = coral
+
+def depredation(reef, Fd, pd, N, M):
+    #find health "cutoff" for corals (used to find corals with worst health)
+    healths = []
+    for i in range(N):
+        for j in range(M):
+            if (not reef[i][j] == None):
+                h = reef[i][j][1]
+                healths.append(h)
+
+    healths.sort()
+    mh = healths[round(len(healths)*Fd)]
+
+    for i in range(N):
+        for j in range(M):
+            if (not reef[i][j] == None):
+                r = random.random()
+                if (r <= pd):
+                    c = reef[i][j]
+                    if (c[1] <= mh):
+                        reef[i][j] = None
+
+#pk = fraction of broadcast spawners
+#k = number of times corals attempt to settle before giving up
+#fa = fraction of corals that asexually reproduce
+#pd = probability for depredation
+def CRO(init, N, M, hf, crossover, mutate, p0, pk, k, Fa, Fd, pd, numIters):
+    assert 0 < p0 and p0 < 1 and 0 < pk and pk < 1 and 0 < pd and pd < 1
+    assert 0 < Fa and Fa < 1 and 0 < Fd and Fd < 1
+    assert 0 < N and 0 < M and 0 <= numIters and 0 < k
+    assert type(init) == list
+    assert len(init) == N * M
+    assert Fa + Fd <= 1
+
+    reef = [([None] * M) for row in range(N)]
+    initialize(reef, init, N, M, p0, hf)
+
     reef_evolutions = []
-    for i in range(numIters):
-        # 0.5, 10, 0.1, 0.05
-        step(hf, reef, pk, k, fa, pd, N, M, crossover, mutation)
+    for _ in range(numIters):
+        bsp, br = separate(reef, pk)
+
+        newCorals = []
+
+        broadcastSpawning(bsp, crossover, newCorals, hf)
+
+        brooding(br, mutate, newCorals, hf)
+
+        larvaeSetting(newCorals, k, reef, N, M)
+
+        # TODO come back!! check paper
+        asexualReproduction(reef, Fa, k, N, M)
+
+        depredation (reef, Fd, pd, N, M)
+
         reef_evolutions.append(np.array(reef))
 
-    flat = [c for row in reef for c in row]
-    solution = max(flat, lambda x: x[1])
+    flatten = [c for row in reef for c in row]
+    solution = max(flatten, lambda x: x[1])
     
     return solution, reef_evolutions
